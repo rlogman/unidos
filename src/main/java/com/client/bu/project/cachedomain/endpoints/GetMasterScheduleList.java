8358c0278261868d903e497b90a9de5bed37d6f8
Rodrigo Lopez-Guzman <rodrigo.lopez@globant.com>
2015-11-03 13:24:04 -0600

    Fixing master schedule endpoint; yet to be completed

--- src/main/java/com/client/bu/project/cachedomain/endpoints/GetMasterScheduleList.java
+++ src/main/java/com/client/bu/project/cachedomain/endpoints/GetMasterScheduleList.java
@@ -5,7 +5,12 @@
  */
 package com.client.bu.project.cachedomain.endpoints;
 
-import com.client.bu.project.cachedomain.servicedescriptor.BooleanType;
+import static com.client.bu.project.cachedomain.util.CrewSchedulingUtils.getDomicileType;
+import static com.client.bu.project.cachedomain.util.CrewSchedulingUtils.getMasterScheduleAlertType;
+import static com.client.bu.project.cachedomain.util.CrewSchedulingUtils.getMasterScheduleFlightSegmentType;
+import static com.client.bu.project.cachedomain.util.CrewSchedulingUtils.getMasterScheduleNonFlyingSegmentType;
+
+import com.client.bu.project.cachedomain.servicedescriptor.CollectionBuilder;
 import com.client.bu.project.cachedomain.servicedescriptor.DateTimeType;
 import com.client.bu.project.cachedomain.servicedescriptor.Field;
 import com.client.bu.project.cachedomain.servicedescriptor.FieldBuilder;
@@ -13,8 +18,6 @@ import com.client.bu.project.cachedomain.servicedescriptor.Int32;
 import com.client.bu.project.cachedomain.servicedescriptor.ObjectBuilder;
 import com.client.bu.project.cachedomain.servicedescriptor.ServiceDescriptorBuilder;
 import com.client.bu.project.cachedomain.servicedescriptor.StringType;
-import com.client.bu.project.cachedomain.util.CrewSchedulingUtils;
-
 /**
  *
  * @author berenice.morales
@@ -23,101 +26,111 @@ import com.client.bu.project.cachedomain.util.CrewSchedulingUtils;
  */
 public class GetMasterScheduleList extends ServiceDescriptorBuilder {
 
-    public GetMasterScheduleList() {
-        this.name("getMasterScheduleList");
-        this.description("");
-        this.urlPattern("");
-        this.returnType(new ObjectBuilder().fields(new Field[]{
-            new FieldBuilder()
-            .name("id")
-            .type(StringType.class)
-            .build(),
-            new FieldBuilder().name("position")
-            .type(Int32.class)
-            .build(),
-            new FieldBuilder().name("seniority")
-            .type(StringType.class)
-            .build(),
-            new FieldBuilder()
-            .name("fullname")
-            .type(StringType.class) // FIXME validate type
-            .build(),
-            new FieldBuilder()
-            .name("domicile")
-            .type(StringType.class)
-            .build(),
-            new FieldBuilder()
-            .name("cellphone")
-            .type(Int32.class)
-            .build(),
-            new FieldBuilder()
-            .name("alertAssociated")
-            .type(StringType.class)
-            .build(),
-            new FieldBuilder()
-            .name("lastUpdated")
-            .type(DateTimeType.UTC_TIMESTAMP_MINUTES)
-            .build(),
-            new FieldBuilder()
-            .name("alertInformation")
-            .type(new ObjectBuilder().fields(new Field[]{
-                new FieldBuilder()
-                .name("severity")
-                .type(StringType.class)
-                .build(),
-                new FieldBuilder().name("alertType")
-                .type(StringType.class)
-                .build(),
-                new FieldBuilder().name("affectedPairings")
-                .type(CrewSchedulingUtils.getPairingCollectionType())
-                .build(),
-                new FieldBuilder().name("crewAffected")
-                .type(StringType.class)
-                .build(),
-                new FieldBuilder()
-                .name("numberFlight")
-                .type(StringType.class)
-                .build(),
-                new FieldBuilder()
-                .name("assignedTo")
-                .type(StringType.class)
-                .build(),
-                new FieldBuilder()
-                .name("watchers")
-                .type(CrewSchedulingUtils.getWatchers())
-                .build(),
-                new FieldBuilder()
-                .name("lastUpdate")
-                .type(DateTimeType.UTC_TIMESTAMP_MINUTES)
-                .build(),
-                new FieldBuilder().name("scheduledDepartureTimestamp")
-                .type(DateTimeType.UTC_TIMESTAMP_MINUTES)
-                .build(),
-                new FieldBuilder().name("actualDepartureTimestamp")
-                .type(DateTimeType.UTC_TIMESTAMP_MINUTES)
-                .build(),
-                new FieldBuilder().name("scheduledArrivalTimestamp")
-                .type(DateTimeType.UTC_TIMESTAMP_MINUTES)
-                .build(),
-                new FieldBuilder().name("actualArrivalTimestamp")
-                .type(DateTimeType.UTC_TIMESTAMP_MINUTES)
-                .build(),}).build())
-            .build(),
-            new FieldBuilder().name("masterSchedule")
-            .type(new ObjectBuilder().fields(new Field[]{
-                new FieldBuilder().name("flightSegments")
-                .type(Int32.class)
-                .build(),
-                new FieldBuilder().name("nonFlyingActivities")
-                .type(StringType.class)
-                .build(),
-                new FieldBuilder().name("flyingAlerts")
-                .type(StringType.class)
-                .build(),
-                new FieldBuilder().name("NoFlyingAlerts")
-                .type(StringType.class)
-                .build(),}).build())
-            .build(),}).build())
-                .build();
-    }
+	public GetMasterScheduleList() {
+		this.name("getMasterScheduleList")
+		.description("")
+		.urlPattern("")
+		.parameters(new Field[] {	// TODO Validate
+				new FieldBuilder().name("crewMemberId")
+						.type(StringType.class)
+						.build(),
+		})
+		.returnType(new CollectionBuilder().itemType(new ObjectBuilder().fields(new Field[] {
+				new FieldBuilder().name("position")
+						.type(StringType.class)
+						.possibleValues(new String[] {
+						})
+						.build(),
+				new FieldBuilder().name("firstName")
+						.type(StringType.class)
+						.build(),
+				new FieldBuilder().name("lastName")
+						.type(StringType.class)
+						.build(),
+				new FieldBuilder().name("domicile")
+						.type(getDomicileType())
+						.build(),
+				new FieldBuilder().name("cellPhone")
+						.type(StringType.class)
+						.build(),
+				new FieldBuilder().name("alertsAssociated")
+						.type(new CollectionBuilder().itemType(new ObjectBuilder().fields(new Field[] {
+								new FieldBuilder().name("flightNumber")
+										.type(StringType.class)
+										.build(),
+								new FieldBuilder().name("severity")
+										.type(StringType.class)
+										.possibleValues(new String[] {
+												// FIXME provide possible values
+										})
+										.build(),
+								new FieldBuilder().name("type")
+										.type(StringType.class)
+										.possibleValues(new String[] {
+												// FIXME provide possible values
+										})
+										.build(),
+								new FieldBuilder().name("pairingsAffected")	// TODO is this an enumeration or a count?
+										.type(new CollectionBuilder().itemType(StringType.class).build())	// TODO Validate
+										.build(),
+								new FieldBuilder().name("affectedFACount")
+										.type(Int32.class)
+										.build(),
+								new FieldBuilder().name("affectedPilotsCount")
+										.type(Int32.class)
+										.build(),
+								new FieldBuilder().name("scheduledDepartureTimestamp")
+										.type(DateTimeType.UTC_TIMESTAMP_MINUTES)
+										.build(),
+								new FieldBuilder().name("scheduledArrivalTimestamp")
+										.type(DateTimeType.UTC_TIMESTAMP_MINUTES)
+										.build(),
+								new FieldBuilder().name("estimatedDepartureTimestamp")
+										.type(DateTimeType.UTC_TIMESTAMP_MINUTES)
+										.build(),
+								new FieldBuilder().name("estimatedArrivalTimestamp")
+										.type(DateTimeType.UTC_TIMESTAMP_MINUTES)
+										.build(),
+								new FieldBuilder().name("actualDepartureTimestamp")
+										.type(DateTimeType.UTC_TIMESTAMP_MINUTES)
+										.build(),
+								new FieldBuilder().name("actualArrivalTimestamp")
+										.type(DateTimeType.UTC_TIMESTAMP_MINUTES)
+										.build(),
+								new FieldBuilder().name("local.assignee")
+										.type(StringType.class)
+										.build(),
+								new FieldBuilder().name("local.watchers")
+										.type(new CollectionBuilder().itemType(StringType.class).build())
+										.build(),
+								new FieldBuilder().name("lastUpdate")
+										.type(DateTimeType.UTC_TIMESTAMP_MINUTES)
+										.build(),
+						})
+						.build()).build())
+						.build(),
+				new FieldBuilder().name("monthsAvailable").type(new CollectionBuilder().itemType(new ObjectBuilder().fields(new Field[] {
+						new FieldBuilder().name("month")
+								.type(DateTimeType.UTC_MONTH)
+								.build(),
+						new FieldBuilder().name("flightSegments")
+								.type(new CollectionBuilder().itemType(getMasterScheduleFlightSegmentType()).build())
+								.build(),
+						new FieldBuilder().name("nonFlyingSegments")
+								.type(new CollectionBuilder().itemType(getMasterScheduleNonFlyingSegmentType()).build())
+								.build(),
+						new FieldBuilder().name("flyingAlerts")
+								.type(new CollectionBuilder().itemType(getMasterScheduleAlertType()).build())
+								.build(),
+						new FieldBuilder().name("nonFlyingAlerts")
+								.type(new CollectionBuilder().itemType(getMasterScheduleAlertType()).build())
+								.build(),
+						}).build()).build())
+						.build(),
+				new FieldBuilder().name("lastUpdate")
+						.type(DateTimeType.UTC_TIMESTAMP_SECONDS)
+						.build(),
+		}).build()).build())
+		.build();
+	}
 }
