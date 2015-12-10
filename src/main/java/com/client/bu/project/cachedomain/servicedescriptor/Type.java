2a1fb304530d1b8a4a26c299a75d6f9f64b41b2e
Rodrigo Lopez-Guzman <rodrigo.lopez@globant.com>
2015-12-10 08:41:27 -0600

    Excluding objectSample type property from JSON output

--- src/main/java/com/client/bu/project/cachedomain/servicedescriptor/Type.java
+++ src/main/java/com/client/bu/project/cachedomain/servicedescriptor/Type.java
@@ -1,6 +1,10 @@
 package com.client.bu.project.cachedomain.servicedescriptor;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 public abstract class Type {
   public abstract String getKind();
+
+  @JsonIgnore
   public abstract Object getObjectSample();
 }
